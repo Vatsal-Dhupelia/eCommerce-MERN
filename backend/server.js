9b81b31313782729c5d6f66756a7d67bcfde4238
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-23 19:54:06 +0530

    Server and Database Setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+
+const dotenv = require("dotenv")
+
+const connectDatabase = require("./config/database")
+
+//Config
+dotenv.config({path: "backend/config/config.env"})
+
+//Connecting to database : 
+connectDatabase();
+
+
+
+app.listen(process.env.PORT, ()=>{
+
+    console.log("Server is working");
+})
