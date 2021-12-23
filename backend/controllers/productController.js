15121722e75a99df6085b96998206448e5d8a05a
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-24 01:32:13 +0530

    Performed Routing for Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,79 @@
-exports.getAllProducts = (req, res)=>{
+const Product = require("../models/productModel")
 
-    res.status(200).json({message: "Route Setup"})
+
+//Create Product - Admin
+exports.createProduct = async (req, res, next)=>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product 
+    })
+}
+
+
+//Get all Products
+exports.getAllProducts = async (req, res)=>{
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+//Update Product - Admin
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, 
+        runValidators: true, 
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Delete Product
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully"
+    })
+}
+
+// Get Product Details
+
+exports.getProductDetails = async(req, res, next)=> {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts)
+router.route("/products").get(getAllProducts);
+router.route("/products/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 module.exports = router
+
+//Stopped Video at 01.02.40
