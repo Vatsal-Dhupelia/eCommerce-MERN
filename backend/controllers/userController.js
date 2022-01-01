c3c3d239abe6849f0ec20827b0bc9a9047510150
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2022-01-01 15:45:12 +0530

    Implemented Password Hashing and Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,8 +15,44 @@ exports.registerUser = catchAsyncErrors(async(req, res, next)=>{
         }
     });
 
+    const token = user.getJWTToken();
+
     res.status(201).json({
         success: true,
-        user,
+        token,
     });
+});
+
+
+// Login User
+
+exports.loginUser = catchAsyncErrors(async(req, res, next)=>{
+
+    const {email, password} = req.body;
+
+    // Checking if user has given password and email both
+
+    if(!email || !password){
+        return next(new ErrorHander("Please enter email and password", 400))
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
+        return next(new ErrorHander("Invalid Email or Password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHander("Invalid Email or Password", 401));
+    }
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+    });
+
 })
