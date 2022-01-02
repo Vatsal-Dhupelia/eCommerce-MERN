ed9dc62ad1d136fa0d5209a05180abae7ca1cf91
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2022-01-02 20:36:52 +0530

    Admin Control for Delete and Update User Roles

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -207,10 +207,46 @@ exports.getAllUser = catchAsyncErrors(async(req, res, next) => {
 exports.getSingleUser = catchAsyncErrors(async(req, res, next) => {
     const user = await User.findById(req.params.id);
     if(!user){
-        return next(new ErrorHander(`User does not exist with ID: ${req.params.id}`))
+        return next(new ErrorHander(`User does not exist with ID: ${req.params.id}`, 400))
     }
     res.status(200).json({
         success: true,
         user,
     });
+});
+
+// Update User Role - Admin
+exports.updateUserRole = catchAsyncErrors(async(req, res, next) => {
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    }
+    
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+    })
+});
+
+// Delete User -- Admin
+exports.deleteUser = catchAsyncErrors(async(req, res, next) => {
+
+    const user = await User.findById(req.params.id);
+    // We will remove cloudinary later
+    if(!user){
+        return next(new ErrorHander(`User does not exist with ID: ${req.params.id}`, 400))
+    }
+
+    await user.remove(); 
+    res.status(200).json({
+        success: true,
+        message: "User Deleted Successfully"
+    })
 });
