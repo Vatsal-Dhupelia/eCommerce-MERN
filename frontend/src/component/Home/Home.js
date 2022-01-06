043b577db2bbfe7ce109b362c25ae86b49660457
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2022-01-06 12:35:53 +0530

    Implementation of Product Card for Product Items

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import "./Home.css"
+import Product from "./Product.js"
+
+const product = {
+    name: "Blue Tshirt",
+    images: [{url: "https://i.ibb.co/DRST11n/1.webp"}],
+    price: "₹3000",
+    _id: "Vatsal"
+}
+
+
 const Home = () => {
     return (
         <>
@@ -10,6 +20,15 @@ const Home = () => {
                     <input class="form-control me-3" type="search" placeholder="Search Products" aria-label="Search"/>
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
+            </div>
+
+            <h2 className="homeHeading">
+                Featured Products
+            </h2>
+
+            <div className="container" id='container'>
+                <Product product = {product}/>
+                
             </div>
         </>
     )
