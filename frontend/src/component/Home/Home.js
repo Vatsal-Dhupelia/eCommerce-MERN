b66f7f22d905181f020341d871674b0fb0432291
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2022-01-08 10:36:07 +0530

    Implemented code for showing Loader

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,38 +1,45 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import "./Home.css"
 import Product from "./Product.js"
 import MetaData from '../layout/MetaData'
-import {getProduct} from "../../actions/productAction"
-import {useSelector, useDispatch} from "react-redux"
+import { getProduct } from "../../actions/productAction"
+import { useSelector, useDispatch } from "react-redux"
+import Loader from '../layout/Loader/Loader'
 
 const Home = () => {
     const dispatch = useDispatch()
-    const {loading, error, products, productsCount} = useSelector((state) => state.products)
+    const { loading, error, products, productsCount } = useSelector((state) => state.products)
     useEffect(() => {
         dispatch(getProduct());
     }, [dispatch])
     return (
         <>
-            <MetaData title="eCart"/>
-            <div className="banner">
-                <h1>eCart</h1>
-                <h2>Quality Product is Our Habit !!</h2>
-                <form className="d-flex">
-                    <input className="form-control me-3" type="search" placeholder="Search Products" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    <MetaData title="eCart" />
+                    <div className="banner">
+                        <h1>eCart</h1>
+                        <h2>Quality Product is Our Habit !!</h2>
+                        <form className="d-flex">
+                            <input className="form-control me-3" type="search" placeholder="Search Products" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
 
-            <h2 className="homeHeading">
-                Featured Products
-            </h2>
+                    <h2 className="homeHeading">
+                        Featured Products
+                    </h2>
 
-            <div className="container" id='container'>
-                {products && products.map(product => (
-                    <Product product = {product}/>
-                ))}
-                
-            </div>
+                    <div className="container" id='container'>
+                        {products && products.map(product => (
+                            <Product product={product} />
+                        ))}
+
+                    </div>
+                </>
+            )}
         </>
     )
 }
